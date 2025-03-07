b8fdb256fabad91133c1f352b1c215d2ec116490
arya <aryapawar92@gmail.com>
2025-03-07 23:28:02 +0530

    last

--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React, { useRef } from "react";
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Text, Billboard } from "@react-three/drei";
-import { TextureLoader } from "three";
+
 import * as THREE from "three";
 import EarthMaterial from "@/components/EarthMaterial";
 import Starfield from "@/components/Starfield";
